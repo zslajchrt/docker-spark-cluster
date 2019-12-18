85ce990f0a2c23769fbb456b59670eb1e10efc9d
Zbynek Slajchrt <zbynek.slajchrt@oracle.com>
2019-12-18 17:49:00 +0100

    GNUR installed

--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -12,4 +12,4 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+cd /spark/bin && /spark/sbin/../bin/spark-class -Xmx4g -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.rmi.port=9011 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
